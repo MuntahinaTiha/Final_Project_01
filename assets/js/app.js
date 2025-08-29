3cf131bb5de3c751ec242e8f2e18146f66bc867a
muntahina <muntahinaislamtiha@gmail.com>
2025-08-29 23:27:17 +0600

    card

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,44 @@
 // MOBILE SEARCH
-let footerMobileSearch = document.querySelector(".footer_mobile_search");
-let mobileSearch = document.querySelector('#mobile_search');
-let searchClose = document.querySelector('.close');
+$(function () {
+    let footerMobileSearch = document.querySelector(".footer_mobile_search");
+    let mobileSearch = document.querySelector('#mobile_search');
+    let searchClose = document.querySelector('.close');
 
-footerMobileSearch.addEventListener('click', function () {
-    mobileSearch.classList.add('mobile_search_active')
+    footerMobileSearch.addEventListener('click', function () {
+        mobileSearch.classList.add('mobile_search_active')
+    });
+    searchClose.addEventListener('click', function () {
+        mobileSearch.classList.remove('mobile_search_active')
+    });
+
+
+    //SHOPERY SLICK SLIDER
+    $('.parent_slider').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        nextArrow: `<span class="right"><iconify-icon icon="tabler:arrow-right" width="24" height="24"></iconify-icon></span>`,
+        prevArrow: `<span class="left"><iconify-icon icon="tabler:arrow-left" width="24" height="24"></iconify-icon></span>`,
+    });
+
+
+    // CATEGORY FILTER
+    $('.category-button').categoryFilter();
+
+
+    // Enable tooltips 
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+
+
+
+
+
+
+
 });
-searchClose.addEventListener('click', function () {
-    mobileSearch.classList.remove('mobile_search_active')
-});
-// MOBILE SEARCH END
+
 
 
